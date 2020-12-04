00b95e3941502fff5fed0445069d68de386f537e
David Rafe <david.rafe@gmail.com>
2020-12-04 17:29:08 -0500

    commit tree a6d206b16d75fd78e0d60b8e2c4474dee127056b

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,54 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    @@all << self 
+  end
+  def say_species
+    "I am a human."
+  end
+  def self.all
+    @@all 
+  end
+  def self.count
+    @@all.count
+  end
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}  
+  end
+  def dogs
+    Dog.all.select {|dog| dog.owner == self} 
+  end
+  def buy_cat(name)
+    new_cat = Cat.new(name, self)
+    self.cats 
+  end
+  def buy_dog(name)
+    new_dog = Dog.new(name, self)
+    self.dogs 
+  end
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}  
+  end
+  def sell_pets
+    self.cats.each {|cat| cat.mood = "nervous"}  
+    self.cats.each {|cat| cat.owner = nil}
+    self.dogs.each {|dog| dog.mood = "nervous"}
+    self.dogs.each {|dog| dog.owner = nil} 
+  end
+  def list_pets
+    cats = self.cats.count 
+    dogs = self.dogs.count 
+    "I have #{dogs} dog(s), and #{cats} cat(s)."
+  end
 end
